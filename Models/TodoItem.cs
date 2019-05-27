05ca73148ac59e9acd1862d01834b4178e18c388
Bruno Buck <b.brigagao@gmail.com>
2019-05-27 17:34:02 -0300

    Adding no arguments constructor that was missing

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -4,7 +4,7 @@ namespace TodoApi.Models
         public long id { get; set; }
         public string name { get; set; }
         public bool isComplete { get; set; }
-
+        public TodoItem() {}
         public TodoItem(string name, bool isComplete) {
             this.name = name;
             this.isComplete = isComplete;
